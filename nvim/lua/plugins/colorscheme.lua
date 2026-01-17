6ca50a4cfc830df7f099e1e30a7ec08e98d82547
frozen-infinity <104211963+frozen-infinity@users.noreply.github.com>
2026-01-17 16:55:03 +0300

    Add files via upload

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add gruvbox
+  { "andersevenrud/nordic.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nordic",
+    },
+  },
+}
